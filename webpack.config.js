857be8a61c4e69f577e15c6f60962af367ea6b03
Andrii Stiahailo <kasjewelfr@gmail.com>
2017-06-20 12:03:21 +0300

    A stable version is developed, but the minisation of js does not work

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,52 @@
 const path = require('path');
+const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const merge = require('webpack-merge');
 const devserver = require('./webpack/devserver');
+const sass = require('./webpack/sass');
+const css = require('./webpack/css');
+const extractCSS = require('./webpack/css.extract');
+//const uglifyJS = require('./webpack/js.uglify');
+const images = require('./webpack/images');
 
 const PATHS = {
     source: path.join(__dirname, 'source'),
     build: path.join(__dirname, 'build')
 };
 
-const common = {
-    entry: PATHS.source + '/index.js',
-    output: {
-        path: PATHS.build,
-        filename: '[name].js'
+const common = merge([
+    {
+        entry: PATHS.source + '/index.js',
+        output: {
+            path: PATHS.build,
+            filename: 'js/[name].js'
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                title: 'Webpack app'
+            }),
+            new webpack.optimize.CommonsChunkPlugin({
+                name: 'common'
+            })
+        ]
     },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'Webpack app'
-        })
-    ]
-};
+    images()
+]);
 
 module.exports = function (env) {
     if (env === 'production') {
-        return common;
+        return merge([
+            common,
+            extractCSS(),
+            // uglifyJS() don't work
+        ])
     }
     if (env === 'development') {
         return merge([
             common,
-            devserver()
+            devserver(),
+            sass(),
+            css()
             ])
     }
 };
